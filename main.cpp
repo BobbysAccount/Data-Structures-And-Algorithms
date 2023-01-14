2888167c218e3ef0e75d359cb5d72e7979a1f17f
Louis Platt <louisjplatt@hotmail.co.uk>
2023-01-14 16:46:31 +0000

    adding the main file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+void NumDisplay(std::string phrase, int num2)
+{
+    std::cout << phrase << num2 << std::endl;
+}
+
+int main() //main function
+{
+
+    
+    NumDisplay("My favourite number ", 23);
+    
+
+    return 0;  
+}
