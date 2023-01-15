67bc126f2a6c9cfb17722c5e4c433c890da0d3fc
Louis Platt <louisjplatt@hotmail.co.uk>
2023-01-15 17:07:34 +0000

    added basic sorting algorithm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#define MAX 100
 
 // this prints text
 void Print(std::string phrase)
@@ -8,11 +10,51 @@ void Print(std::string phrase)
 }
 
 
+
 int main() //main function
 {
+    int n;
+    int array[MAX];
+    Print("Enter max array limit: ");
+    std::cin >> n;
 
+    // inputting values in an array
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rand();
+    }
     
-    Print("My favourite number\n");
+    // outputting the unsorted array
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    Print("\n");
+
+    // sorting an array
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            // asscending order
+            if (array[j]<array[i])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            
+        }
+        
+    }
+    
+    // display the sorted array
+    Print("Sorted");
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << array[i] << " ";
+    }
+    std::cout << std::endl;
     
 
     return 0;  
