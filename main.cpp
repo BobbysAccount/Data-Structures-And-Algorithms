01695f5a03482d2743acdea7e3319035a65bc3f3
BobbysAccount <louisjplatt@hotmail.co.uk>
2023-02-01 20:03:27 +0000

    added two files

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <algorithm>
 
-#define MAX 100
+#include "algorithms.hpp"
 
 // this prints text
 void Print(std::string phrase)
@@ -14,32 +14,11 @@ void Print(std::string phrase)
 
 int main() //main function
 {
-    int n;
-    int array[MAX];
-    Print("Enter max array limit: ");
-    std::cin >> n;
 
-    // inputting values in an array
-    for (int i = 0; i < n; i++)
-    {
-        array[i] = rand();
-    }
-    Print("Unsorted");
-    // outputting the unsorted array
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << array[i] << " ";
-    }
-    Print("\n");
+    
 
-    std::sort(array, array+n);
-    // display the sorted array
-    Print("Sorted");
-    for (int i = 0; i < n; i++)
-    {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
+
+    Print("Hello!");
     
     return 0;  
 }
